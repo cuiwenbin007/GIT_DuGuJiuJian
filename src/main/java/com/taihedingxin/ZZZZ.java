1f9499f468949c56e97a64ffd9a54d0041274dad
cuiwenbin <892174804@qq.com>
2019-06-14 19:54:51 +0800

    修改1222

---
+++ src/main/java/com/taihedingxin/ZZZZ.java
@@ -0,0 +1,11 @@
+package com.taihedingxin;
+
+/**
+ * @author CuiNeal
+ * @date 2019/6/14 19:54
+ */
+public class ZZZZ {
+    public static void main(String[] args) {
+        System.out.println("ZZZZZZZZZZZZZZZ");
+    }
+}
